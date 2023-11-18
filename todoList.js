23df477b9bbc6f00de4959515b683f0914d69088
Juvens Laurensius Dwitama <juvenslaurensiusdwitama@gmail.com>
2023-11-18 15:03:36 +0700

    .

--- todoList.js
+++ todoList.js
@@ -3,20 +3,36 @@ const submit = document.querySelector('.input-button button')
 const taskList = document.querySelector('.taskList')
 const card = document.querySelector('.card')
 const task = document.querySelector('.task')
+const sb = document.querySelector('.sb')
+
 submit.addEventListener('click', () =>{
     if(textInput.value === ''){
         alert('There is no task added!')
     }else{
-        let div = document.createElement('div')
+        let div1 = document.createElement('div')
+        let div2 = document.createElement('div')
         let cBox = document.createElement('input')
+        let taskDiv = document.createElement('div')
         cBox.type = 'checkbox'
         let p = document.createElement('p')
+        let x = document.createElement('p')
+        x.innerText = 'x'
+        x.classList.add('x')
         p.innerHTML = textInput.value
-        div.append(cBox,p)
-        div.classList.add('task')
-        taskList.append(div)
+        div1.append(cBox,p)
+        div2.append(x)
+        div1.classList.add('task')
+        taskDiv.append(div1,div2)
+        taskDiv.classList.add('sb')
+        taskList.append(taskDiv)
     }
     textInput.value = ''
+})
+
+card.addEventListener('click', e =>{
+    if(e.target.className == 'x'){
+        e.target.parentElement.parentElement.remove()
+    }
 })
 
 
